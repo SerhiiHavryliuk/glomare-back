ac3bb9fde5a00b38e8f887a8fac2e6a47d04888e
Serhii <havryliuk.serhii@icloud.com>
2022-07-16 15:38:41 +0300

    восстановил их резервной копии

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import {MongooseModule} from "@nestjs/mongoose";
+import {MongooseModule} from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestProductModule } from './test-product/test-product.module';
-import { AppGateway } from './app.gateway';
 
 @Module({
   imports: [TestProductModule, MongooseModule.forRoot('mongodb+srv://serhii:12345@cluster0.hr9dt.mongodb.net/?retryWrites=true&w=majority')],
   controllers: [AppController],
-  providers: [AppService, AppGateway],
+  providers: [AppService],
 })
 export class AppModule {}

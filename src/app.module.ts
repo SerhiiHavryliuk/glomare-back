f4dfab0cf2e4d760485cb332094420ac71bdf20a
Serhii <havryliuk.serhii@icloud.com>
2022-07-27 17:47:38 +0300

    fixed branch Dmutro Ludin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,40 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TestProductModule } from './test-product/test-product.module';
+import { NewsModule } from './news/news.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModule } from './users/users.module';
+import { PassportModule } from '@nestjs/passport';
 import { AppGateway } from './app.gateway';
+import { StatisticsModule } from './statistics/statistics.module';
+import { Top10Module } from './top10/top10.module';
+import { PremiumModule } from './premium/premium.module';
+import { PartnersModule } from './partners/partners.module';
 
 @Module({
 	imports: [
-		TestProductModule,
+		NewsModule,
 		ConfigModule.forRoot(),
 		MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.hr9dt.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`),
+		StatisticsModule,
+		Top10Module,
+		PremiumModule,
+		AuthModule,
+		PassportModule,
+		UsersModule,
+		PartnersModule
 	],
 	controllers: [
-		AppController]
+		AppController,
+	]
 	,
 	providers: [
 		AppService,
-		AppGateway
+		AppGateway,
+		JwtService,
 	],
 })
-export class AppModule {
-}
+export class AppModule {}
